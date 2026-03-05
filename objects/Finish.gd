1db5ce5097e79a485bb0ca8041ecbda8b9842247
mariana4453 <dudem2@gmail.com>
2026-03-05 02:28:55 +0100

    finish panel fade in
    finish panel fade in

--- objects/Finish.gd
+++ objects/Finish.gd
@@ -2,16 +2,19 @@ extends Area2D
 
 onready var finish_sound = $FinishSound
 onready var finish_panel = $"../../../Gui/FinishPanel"
+onready var animation_player_finish_panel = $"../../../Gui/FinishPanel/AnimationPlayer"
 
 func _ready():
 	finish_sound.pitch_scale = Global.adjust_pitch(finish_sound)
+
+	animation_player_finish_panel.playback_speed = Global.speed
 
 func _on_Finish_area_entered(area):
 	if area.is_in_group("player"):
 		var player = area
 
 		player.can_move = false
-		
+
 		finish_sound.play()
-		
-		finish_panel.visible = 1
+
+		animation_player_finish_panel.play("fade_in")
